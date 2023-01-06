084c34298484cfd5f7fa8a9c46dd333a6059f105
Vera Aparecida Dos Santos <vera.a.dossantos@br.infra.d>
2023-01-06 09:47:05 -0300

    alteracoes api test

--- src/test/java/br/ce/wcaquino/tasks/apitest/APITest.java
+++ src/test/java/br/ce/wcaquino/tasks/apitest/APITest.java
@@ -47,4 +47,26 @@ public class APITest {
 			.body("message", CoreMatchers.is("Due date must not be in past"))
 		;
 	}
+	
+	@Test
+	public void deveRemoverTarefaComSucesso() {
+		Integer id = RestAssured.given()
+			.body("{\"task\":\"Tarefa teste\",\"dueDate\":\"2030-12-30\"}")
+			.contentType(ContentType.JSON)
+		.when()
+			.post("/todo")
+		.then()
+			//.log().all()
+			.statusCode(201)
+			.extract().path("id")
+		;
+		System.out.println(id);
+		
+		// Remover
+		RestAssured.given()
+		.when()
+			.delete("/todo/"+id)
+			.then()
+				.statusCode(204);
+	}
 }
